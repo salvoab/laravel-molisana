d15bed8c2a2da16c04536b532e6be56df0bb30a6
salvoab <salvatore.abate85@gmail.com>
2021-02-11 18:52:50 +0100

    Visualizzati tutti i prodotti con l'uso di parziali
    Ogni categoria di prodotto è stata inserita in un parziale blade.php. I parziali sono stati inclusi in prodotti.blade.php

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <!-- <img src="{{ $paste[0]->src }}" alt=""> -->
+    
     @php
         $paste_lunghe = Arr::where($paste, function($pasta, $chiave) {
             return $pasta->tipo == 'lunga';
@@ -21,20 +21,8 @@
     @endphp
 
     <div class="prodotti">
-        <!-- {{ var_dump($paste_lunghe['0']) }}   -->
-        <section class="paste-lunghe">
-            <h2>LE LUNGHE</h2>
-            <div class="container">
-                @foreach($paste_lunghe as $pasta)
-                    <div class="pasta">
-                        <img class="pasta-img" src="{{ $pasta->src }}" alt="{{ $pasta->titolo }}">
-                        <div class="overlay">
-                            <h3>{{ $pasta->titolo }}</h3>
-                            <img class="icon" src="{{ asset('img/icon.svg') }}" alt="">
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </section>
+        @include('partials.paste_lunghe')
+        @include('partials.paste_corte')
+        @include('partials.paste_cortissime')
     </div>
 @endsection
